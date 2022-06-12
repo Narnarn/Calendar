bd6149f8c10f553b8c805f71418d58fa02625f02
Narnarn <keyra32@gmail.com>
2022-06-12 16:50:53 +0800

    better appearence for date

--- components/CalenderHeader.js
+++ components/CalenderHeader.js
@@ -20,24 +20,32 @@ function CalenderHeader() {
   }
   return (
     <header className="px-4 py-2 flex items-center">
+      <div className="absolute left-7 z-10 w-6 h-5  bg-white text-[#4285F4]  font-bold text-xl text-center leading-[20px]">
+        {dayjs().date()}
+      </div>
+      {/* <div className="absolute text-[#4285F4] font-bold text-xl left-7 z-10">
+        
+      </div> */}
       <Image
-        src="/logo.png"
+        src="/cal.svg"
         alt="logo"
         className="mr-4"
         width={48}
         height={48}
       />
-      <h1 className="mr-10 text-xl text-gray-500 font-bold"> Calender</h1>
+      <h1 className="mr-10 ml-4 text-xl text-gray-500 font-bold">
+        My Calendar
+      </h1>
       <button onClick={handleReset} className="border rounded py-2 px-4 mr-5">
         Today
       </button>
       <button onClick={handlePrevMonth}>
-        <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+        <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2 mt-2">
           chevron_left
         </span>
       </button>
       <button onClick={handleNextMonth}>
-        <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+        <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2 mt-2">
           chevron_right
         </span>
       </button>
